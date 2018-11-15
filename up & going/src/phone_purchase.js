019f9a60d296908b44dad6ee4b68f499e3a36e3a
realtimschmidt <schmidt.tim.j@gmail.com>
2018-11-14 19:30:46 -0500

    wip set up jest and package.json so I can TDD while practicing with YDKJ

---
+++ up & going/src/phone_purchase.js
@@ -0,0 +1,33 @@
+const tax = 0.07
+
+const addTax = (amount) => {
+  return amount + (amount * tax)
+}
+
+const dollarFormatter = (unformattedValue) => {
+  const formatDecimals = unformattedValue.toFixed(2)
+  return `$${formatDecimals}`
+}
+
+const phonePurchase = (bankAccount) => {
+  // const bankAccount = 1000
+  const phonePrice = 199
+  const phoneAccessories = 50
+
+  let subTotal
+
+  subTotal = 0
+  
+  while ((subTotal + phonePrice + phoneAccessories) * (1 + tax) <= bankAccount) {
+    subTotal += phonePrice + phoneAccessories
+  }
+  
+  const totalAfterTax = addTax(subTotal)
+  const formattedTotal = dollarFormatter(totalAfterTax)
+  
+  console.log(formattedTotal)
+
+  return formattedTotal
+}
+
+phonePurchase(1000)
