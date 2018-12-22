503ffe6c83a14e0429b3461d8d371438b44429ac
realtimschmidt <schmidt.tim.j@gmail.com>
2018-12-22 06:26:12 -0500

    more call statck practice

--- this & object prototypes/src_practice/ch2_01.js
+++ this & object prototypes/src_practice/ch2_01.js
@@ -1,11 +1,13 @@
 const baz = () => {
   console.log("baz")
   bar()
+  console.log("last message")
 }
 
 const bar = () => {
   console.log("bar")
   foo()
+  console.log("after foo")
 }
 
 const foo = () => {
