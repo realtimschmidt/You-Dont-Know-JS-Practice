92cad4f95e7affd3fd60afeb03df227616708e17
realtimschmidt <schmidt.tim.j@gmail.com>
2018-12-26 14:30:36 -0600

    define property with desired characteristics

--- this & object prototypes/src_practice/ch3_descriptors.js
+++ this & object prototypes/src_practice/ch3_descriptors.js
@@ -3,3 +3,14 @@ var myObj = {
 }
 
 console.log(Object.getOwnPropertyDescriptor( myObj, "a"))
+
+var myObj2 = {}
+
+Object.defineProperty( myObj2, "a", {
+  value:2,
+  writable: true,
+  configurable: true,
+  enumerable: true
+})
+
+console.log(myObj2.a)
