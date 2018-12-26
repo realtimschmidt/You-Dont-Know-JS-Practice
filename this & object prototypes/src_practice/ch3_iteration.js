ce985cd3a9baf33adbae914f84238584871ab416
realtimschmidt <schmidt.tim.j@gmail.com>
2018-12-26 17:32:24 -0600

    manually iterating over array

--- this & object prototypes/src_practice/ch3_iteration.js
+++ this & object prototypes/src_practice/ch3_iteration.js
@@ -3,3 +3,17 @@ const myArray = [1, 2, 3]
 for (let i = 0; i < myArray.length; i++) {
   console.log( myArray[i])
 }
+
+for (let index of myArray) {
+  console.log(index)
+}
+
+let it
+
+it = myArray[Symbol.iterator]()
+
+console.log(
+  it.next(),
+  it.next(),
+  it.next(),
+  it.next())
