299a3e4ffa924de0dfb28ae5d4b44ae122cee8ee
realtimschmidt <schmidt.tim.j@gmail.com>
2019-01-01 18:00:32 -0500

    prototype shadowing

--- this & object prototypes/src_practice/ch5_prototypes.js
+++ this & object prototypes/src_practice/ch5_prototypes.js
@@ -13,3 +13,18 @@ for (var k in myObject){
 }
 
 console.log('a' in myObject)
+
+
+// Shadowing: 
+// Setting a property name on an object that already exists in an object higher in the prototype chain.
+//
+// If the property higher in the prototype chain is read only (writable:false) then the new property in the object lower on the prototype chain will not be allowed
+
+console.log(anotherObject.hasOwnProperty("a"))
+console.log(myObject.hasOwnProperty("a"))
+
+console.log(myObject.a++) // implicit shadowing
+console.log(anotherObject.a)
+console.log(myObject.a)
+
+console.log(myObject.hasOwnProperty("a"))
