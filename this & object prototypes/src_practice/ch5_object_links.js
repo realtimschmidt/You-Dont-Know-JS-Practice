29a67992690182c825192f4220a8fed0ebd915c0
realtimschmidt <schmidt.tim.j@gmail.com>
2019-01-01 21:15:19 -0500

    a little easier to read

--- this & object prototypes/src_practice/ch5_object_links.js
+++ this & object prototypes/src_practice/ch5_object_links.js
@@ -5,3 +5,20 @@ const foo = {
 const bar = Object.create( foo )
 
 bar.something()
+
+
+
+// possibly easier to read
+const anotherObject = {
+  cool: () =>  {
+    console.log( "cool!" )
+  }
+}
+
+const myObject = Object.create( anotherObject )
+
+myObject.doCool = function() {
+  this.cool()
+}
+
+myObject.doCool()
